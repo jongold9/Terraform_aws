1793c6ac7314eb3435aaab7f8aeaa665f4b95a5e
vair <vm@air.local>
2024-03-10 14:11:54 +0200

    add var.aws_region

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 # Объявление переменных
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "eu-central-1"
+}
+
 variable aws_access_key {
   type    = string
   description = "AWS Access Key"
